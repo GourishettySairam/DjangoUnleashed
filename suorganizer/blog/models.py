992f7b083e88d684f748a5a1c01777dbc771e283
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-02-27 22:59:02 +0530

    added parameter options to models

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -4,9 +4,24 @@ from organizer.models import Startup, Tag
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=63, 
+                help_text='A label for URL config',
+                unique_for_month='pub_date')
     text = models.TextField()
-    pub_date = models.DateField()
+    pub_date = models.DateField(
+        'date_published',
+        auto_now_add=True   #  automatically set to the current date the ﬁrst time it is submitted to the database
+    )
 
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup
+    # Finally, we want to be able to write blog posts about startups. 
+    # Each blog post may be about multiple startups, 
+    # and each startup may be written about multiple times, 
+    # another many-to-many relationship
+    tags = models.ManyToManyField(
+        Tag, 
+        related_name='blog_posts'
+    )
+    startups = models.ManyToManyField(
+        Startup, 
+        related_name='blog_posts'
+    )
--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -6,12 +6,12 @@ class Tag(models.Model):
     slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config. ')
 
 class Startup(models.Model):
-    name = models.CharField(max_length=31)
-    slug = models.SlugField()
+    name = models.CharField(max_length=31, db_index=True)   # index automatically in the database
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config. ')   #unique fields are automatically indexed
     description = models.TextField()
-    founded_date = models.DateField()
+    founded_date = models.DateField('date founded')
     contact = models.EmailField()
-    website = models.URLField()
+    website = models.URLField(max_length=255)
 
     # Each startup will have multiple tags, 
     # and each tag may belong to multiple startups. 
@@ -22,8 +22,8 @@ class Startup(models.Model):
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=63)
-    pub_date = models.DateField()
-    link = models.URLField()
+    pub_date = models.DateField('date published')
+    link = models.URLField(max_length=255)
 
     #  A one-to-many relationship pointing news links to startups
     #  A news link may belong to only one startup, 
@@ -31,11 +31,5 @@ class NewsLink(models.Model):
     #  meaning we seek to create a one-to-many relationship. 
     #  A news link points at a startup: 
     #  a news link has a foreign key to a startup.
-    startup = models.ForeignKey(Startup)
+    startup = models.ForeignKey(Startup, on_delete=models.DO_NOTHING)
 
-
-
-
-
-Each startup will have multiple tags, and each tag may belong to multiple startups. This is a many-to-many relationship. Similarly, each blog post will be associated with tags via a many-to-many relationship
-Finally, we want to be able to write blog posts about startups. Each blog post may be about multiple startups, and each startup may be written about multiple times, another many-to-many relationship
