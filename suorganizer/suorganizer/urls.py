f387ff6ca4b5dc9a68a0f35e2aea54df214307d5
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-11 22:33:32 +0530

    name for url

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -22,5 +22,5 @@ from organizer.views import homepage, tag_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', homepage),
-    url(r'tag/(?P<slug>[\w\-]+)/$', tag_detail,)
+    url(r'tag/(?P<slug>[\w\-]+)/$', tag_detail,name='organizer_tag_detail')
 ]
