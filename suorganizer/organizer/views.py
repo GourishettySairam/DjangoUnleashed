763e2fea0f34b5ab51205711ce5325f3eaf9cbcc
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-13 23:33:50 +0530

    using render method

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (get_object_or_404, render)
 
 from django.http.response import HttpResponse, Http404, HttpResponseNotFound
 from .models import Tag
@@ -6,14 +6,8 @@ from django.template import loader, Context
 # Create your views here.
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = {'tag_list': tag_list}
-    output = template.render(context)
-    return HttpResponse(output)
+    return render(request, 'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
 
 def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug__iexact=slug)
-    template = loader.get_template('organizer/tag_detail.html')
-    context = {'tag': tag}
-    return HttpResponse(template.render(context))
+    return render(request, 'organizer/tag_detail.html', {'tag': tag})
