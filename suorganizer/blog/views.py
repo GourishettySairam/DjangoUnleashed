c584a4f72ecf4ce71d4c6880e2079cb6d6949897
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-15 00:21:24 +0530

    Implemented Class View for post list

--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import View
 from .models import Post
 
 # Create your views here
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {'post_list': Post.objects.all()})
-
+class PostList(View):
+    def get(self, request):
+        return render(request, 'blog/post_list.html', {'post_list': Post.objects.all()})
 
 def post_detail(request, year, month, slug):
     post = get_object_or_404(Post,
