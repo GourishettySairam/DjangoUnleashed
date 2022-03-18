daf4fb9c3c9305404c763c040ea4e021b6f91905
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-19 00:33:44 +0530

    another way to using redirection

--- suorganizer/suorganizer/views.py
+++ suorganizer/suorganizer/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 def redirect_root(request):
     return redirect('blog_post_list')
+
+    # url_path = reverse('blog_post_list')
+    # return HttpResponseRedirect(url_path)
