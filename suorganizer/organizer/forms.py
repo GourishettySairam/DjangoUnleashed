7de977478b378936015dc0532519d586e130f357
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-26 15:37:44 +0530

    preparing to build a clean method for Tag Model slug field

--- suorganizer/organizer/forms.py
+++ suorganizer/organizer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Tag
+from django.core.exceptions import ValidationError
 
 class TagForm(forms.Form):
     name = forms.CharField(max_length=31)
@@ -15,4 +16,8 @@ class TagForm(forms.Form):
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
-    
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == "create":
+            raise ValidationError('Slug may not be "create"')
+        return new_slug
