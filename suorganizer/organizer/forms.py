d0093f4ed2c3e5a52978050605f84d18f6ec6ad6
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-26 15:57:35 +0530

    using ModelForm inheritance to connect TagForm to Tag

--- suorganizer/organizer/forms.py
+++ suorganizer/organizer/forms.py
@@ -2,22 +2,18 @@ from django import forms
 from .models import Tag
 from django.core.exceptions import ValidationError
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(max_length=31, help_text='A label for URL config')
-
-    def save(self):
-        new_tag = Tag.objects.create(
-            name = self.cleaned_data['name'],
-            slug = self.cleaned_data['slug']
-        )
-        return new_tag
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        # fields = "__all__"
+        exclude = ['slug']
+        fields = ['name', 'slug']
     
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
     def clean_slug(self):
-        new_slug = self.cleaned_data['slug'].lower()
+        new_slug = (self.cleaned_data['slug'].lower())
         if new_slug == "create":
             raise ValidationError('Slug may not be "create"')
         return new_slug
