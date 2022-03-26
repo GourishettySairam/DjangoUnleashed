ad7add28c0a9e98d39a2cd50a76ae6425691c86f
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-26 20:44:30 +0530

    Using Mixin to adhere to DRY principle

--- suorganizer/organizer/forms.py
+++ suorganizer/organizer/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from .models import Tag, NewsLink, Startup
 from django.core.exceptions import ValidationError
 
-class TagForm(forms.ModelForm):
+class SlugCleanMixin:
+    """Mixin class for slug cleaning method"""
+
+    def clean_slug(self):
+        new_slug = (
+            self.cleaned_data['slug'].lower()
+        )
+        if new_slug == "create":
+            raise ValidationError('Slug may not be "create"')
+        return new_slug
+class TagForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Tag
         # fields = "__all__"
@@ -12,24 +22,13 @@ class TagForm(forms.ModelForm):
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
-    def clean_slug(self):
-        new_slug = (self.cleaned_data['slug'].lower())
-        if new_slug == "create":
-            raise ValidationError('Slug may not be "create"')
-        return new_slug
 
 class NewsLinkForm(forms.ModelForm):
     class Meta:
         model = NewsLink
         fields = "__all__"
 
-class StartupForm(forms.ModelForm):
+class StartupForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Startup
         fields = "__all__"
-
-    def clean_slug(self):
-        new_slug = (self.cleaned_data['slug'].lower())
-        if new_slug == "create":
-            raise ValidationError('Slug may not be "create"')
-        return new_slug
