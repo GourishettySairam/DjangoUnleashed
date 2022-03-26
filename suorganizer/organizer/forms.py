3bcf483781398030689b7aaf443e263abeed5af1
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-26 20:17:46 +0530

    creating startupform, newslinkform and postform

--- suorganizer/organizer/forms.py
+++ suorganizer/organizer/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Tag
+from .models import Tag, NewsLink, Startup
 from django.core.exceptions import ValidationError
 
 class TagForm(forms.ModelForm):
@@ -11,6 +11,22 @@ class TagForm(forms.ModelForm):
     
     def clean_name(self):
         return self.cleaned_data['name'].lower()
+
+    def clean_slug(self):
+        new_slug = (self.cleaned_data['slug'].lower())
+        if new_slug == "create":
+            raise ValidationError('Slug may not be "create"')
+        return new_slug
+
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = "__all__"
+
+class StartupForm(forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = "__all__"
 
     def clean_slug(self):
         new_slug = (self.cleaned_data['slug'].lower())
