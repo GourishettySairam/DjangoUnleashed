13247120437db685379e6c5279b40eeec5182793
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-06 22:45:31 +0530

    typo fixed

--- suorganizer/organizer/urls.py
+++ suorganizer/organizer/urls.py
@@ -4,7 +4,7 @@ from blog import urls as blog_urls
 
 urlpatterns = [
     url(r'^tag/$', tag_list, name='organizer_tag_list'),
-    url(r'^tag/create/$', TagCreate.as_view(), name='organizer_tag_create')
+    url(r'^tag/create/$', TagCreate.as_view(), name='organizer_tag_create'),
     url(r'tag/(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
     url(r'^startup/$', startup_list, name='organizer_startup_list'),
     url(r'startup/(?P<slug>[\w\-]+)/$', startup_detail, name='organizer_startup_detail'),
