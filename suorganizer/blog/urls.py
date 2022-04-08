747ace6f70d7b17ea2c6b503dac7650477b37566
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-08 23:29:29 +0530

    creating Post objects in a view with PostForm

--- suorganizer/blog/urls.py
+++ suorganizer/blog/urls.py
@@ -5,15 +5,14 @@ urlpatterns = [
     url(r'^$', 
         PostList.as_view(template_name='blog/post_list.html'),
         name="blog_post_list"),
-
+    url(r'^create/$',
+        PostCreate.as_view(),
+        name='blog_post_create'
+    ),
     url(r'^(?P<year>\d{4})/'
     r'(?P<month>\d{1,2})/'
     r'(?P<slug>[\w\-]+)/$',
         post_detail,
         {'parent_template': 'base.html'},
         name="blog_post_detail"),
-    url(r'^create/$',
-        PostCreate.as_view(),
-        name='blog_post_create'
-    )
 ]
--- suorganizer/blog/views.py
+++ suorganizer/blog/views.py
@@ -6,6 +6,29 @@ from django.views.decorators.http import require_http_methods
 from .forms import PostForm
 # Create your views here
 
+
+class PostCreate(View):
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            {'form': self.form_class()})
+
+    def post(self, request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        else:
+            return render(
+                request,
+                self.template_name,
+                {'form': bound_form})
+
+
 class PostList(View):
     template_name = ''
 
@@ -20,27 +43,3 @@ def post_detail(request, year, month, slug, parent_template=None):
         slug=slug
     )
     return render(request, 'blog/post_detail.html', {'post': post, 'parent_template': parent_template})
-
-class PostCreate(View):
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def get(self, request):
-        return render(
-            request,
-            self.template_name,
-            {'form': self.form_class()}
-        )
-    
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_post = bound_form.save()
-            return redirect(new_post)
-        else:
-            return render(
-                request,
-                self.template_name,
-                {'form': bound_form}
-            )
-        
