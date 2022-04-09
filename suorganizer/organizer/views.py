fec8ac2a3f47f48513373dbdaa42b36843e16bb0
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-09 22:42:05 +0530

    shortening organizer views

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -6,6 +6,8 @@ from .forms import NewsLinkForm, TagForm, StartupForm
 from .models import Startup, Tag
 from django.template import loader, Context
 from django.views.generic import View
+
+from .utils import ObjectCreateMixin
 # Create your views here.
 
 def tag_list(request):
@@ -36,68 +38,16 @@ def tag_create(request):
             {'form': form}
         )
 
-class TagCreate(View):
+class TagCreate(ObjectCreateMixin, View):
     form_class = TagForm
     template_name = 'organizer/tag_form.html'
 
-    def get(self, request):
-        return render(
-            request, 
-            self.template_name, 
-            {'form': self.form_class()})
-    
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        else:
-            return render(
-                request,
-                self.template_name,
-                {'form': bound_form}
-            )
 
-class StartupCreate(View):
+class StartupCreate(ObjectCreateMixin, View):
     form_class = StartupForm
     template_name = 'organizer/startup_form.html'
 
-    def get(self, request):
-        return render(
-            request,
-            self.template_name,
-            {'form': self.form_class()})
 
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_startup = bound_form.save()
-            return redirect(new_startup)
-        else:
-            return render(
-                request,
-                self.template_name,
-                {'form': bound_form})
-
-class NewsLinkCreate(View):
+class NewsLinkCreate(ObjectCreateMixin, View):
     form_class = NewsLinkForm
     template_name = 'organizer/newslink_form.html'
-
-    def get(self, request):
-        return render(
-            request,
-            self.template_name,
-            {'form': self.form_class()}
-        )
-    
-    def post(self, request):
-        bound_form = self.form_class(request.POST)
-        if bound_form.is_valid():
-            new_newslink = bound_form.save()
-            return redirect(new_newslink)
-        else:
-            return render(
-                request,
-                self.template_name,
-                {'form': bound_form}
-            )
