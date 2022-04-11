577379bbf90d533f8b9152675ef12f2956fd5a08
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-11 22:34:27 +0530

    creating ObjectDeleteMixin

--- suorganizer/organizer/utils.py
+++ suorganizer/organizer/utils.py
@@ -1,5 +1,6 @@
 from distutils.ccompiler import new_compiler
 from django.shortcuts import redirect, render, get_object_or_404
+from django.http import HttpResponseRedirect
 
 class ObjectCreateMixin:
     form_class = None
@@ -63,3 +64,26 @@ class ObjectUpdateMixin:
                 self.template_name,
                 context
             )
+
+class ObjectDeleteMixin:
+    model = None
+    success_url = ''
+    template_name = ''
+
+    def get(self, request, slug):
+        obj = get_object_or_404(
+            self.model, slug__iexact=slug
+        )
+        context = {
+            self.model.__name__.lower(): obj
+        }
+        return render(
+            request, self.template_name, context
+        )
+
+    def post(self, request, slug):
+        obj = get_object_or_404(
+            self.model, slug__iexact=slug
+        )
+        obj.delete()
+        return HttpResponseRedirect(self.success_url)
