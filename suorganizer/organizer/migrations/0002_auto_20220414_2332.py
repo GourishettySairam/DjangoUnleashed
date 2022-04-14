5e24d7e3a2850b7da1c0eb47c563f0e121c403b0
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-14 23:54:26 +0530

    adding Tag data using migration file

--- suorganizer/organizer/migrations/0002_auto_20220414_2332.py
+++ suorganizer/organizer/migrations/0002_auto_20220414_2332.py
@@ -2,19 +2,43 @@
 
 from django.db import migrations
 
+TAGS = (
+    # ( tag name, tag slug ),
+    ("augmented reality", "augmented-reality"),
+    ("big data", "big-data"),
+    ("django", "django"),
+    ("education", "education"),
+    ("ipython", "ipython"),
+    ("javascript", "javascript"),
+    ("jupyter", "jupyter"),
+    ("mobile", "mobile"),
+    ("node.js", "node-js"),
+    ("php", "php"),
+    ("python", "python"),
+    ("ruby on rails", "ruby-on-rails"),
+    ("ruby", "ruby"),
+    ("video games", "video-games"),
+    ("web", "web"),
+    ("zend", "zend"),
+)
+
+def add_tag_data(apps, schema_editor):
+    Tag = apps.get_model('organizer', 'Tag')
+    for tag_name, tag_slug in TAGS:
+        Tag.objects.create(
+            name=tag_name,
+            slug=tag_slug
+        )
+
+def remove_tag_data(apps, schema_editor):
+    Tag = apps.get_model('organizer', 'Tag')
+    for _, tag_slug in TAGS:
+        tag = Tag.objects.get(slug=tag_slug)
+        tag.delete()
 
 class Migration(migrations.Migration):
 
-    TAGS = (
-        #(tag name, tag slug)
-        ("augmented reality", "augmented-reality")
-    )
-
-    def add_tag_data(apps, schema_editor):
-        pass
-
-    def remove_tag_data(apps, schema_editor):
-        pass
+    
 
     dependencies = [
         ('organizer', '0001_initial'),
