3b6ac13aa02bf41426bc00c83f13bcf3a97ab116
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-14 22:41:55 +0530

    fixing bugs

--- suorganizer/suorganizer/settings.py
+++ suorganizer/suorganizer/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'kolo.middleware.KoloMiddleware',
+    # 'kolo.middleware.KoloMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
