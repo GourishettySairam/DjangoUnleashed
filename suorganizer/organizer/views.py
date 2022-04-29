74b877291032b1849e9d307913f8c47bbedf8639
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-04-30 00:24:23 +0530

    passing next and previous page urls in context

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -9,7 +9,7 @@ from django.template import loader, Context
 from django.views.generic import View
 
 from .utils import ObjectCreateMixin, ObjectDeleteMixin, ObjectUpdateMixin
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -21,22 +21,57 @@ def tag_detail(request, slug):
     return render(request, 'organizer/tag_detail.html', {'tag': tag})
 
 class StartupList(View):
+    page_kwarg = 'page'
     paginate_by = 5
     template_name = 'organizer/startup_list.html'
 
     def get(self, request):
         startups = Startup.objects.all()
         paginator = Paginator(startups, self.paginate_by)
-        page = paginator.page(1)
+        page = None
+        page_number = request.GET.get(
+            self.page_kwarg
+        )
+        prev_url = None
+        next_url = None
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            print(paginator.page(1))
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages
+            )
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n = page.previous_page_number()
+            )
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number()
+            )
+        else:
+            next_url = None
+        
         context = {
+            'is_paginated': page.has_other_pages(),
             'paginator': paginator,
-            'startup_list': page
+            'startup_list': page,
+            'next_page_url': next_url,
+            'previous_page_url': prev_url
         }
         return render(
             request,
             self.template_name,
             context
         )
+
+# Example 14.36 to-do
 
 def startup_detail(request, slug):
     startup = get_object_or_404(Startup, slug__iexact=slug)
