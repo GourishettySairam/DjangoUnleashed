d60b39a1d8659870c65e4671c4a9504b84edc00b
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-05-15 23:29:12 +0530

    added styling to all the templates

--- suorganizer/organizer/urls/tag.py
+++ suorganizer/organizer/urls/tag.py
@@ -3,9 +3,9 @@ from ..views import TagDelete, TagList, TagPageList, tag_detail, tag_list, tag_c
 
 urlpatterns = [
     url(r'^$', TagList.as_view(), name='organizer_tag_list'),
-    url(r'(?P<page_number>\d+)/$', TagPageList.as_view(), name='organizer_tag_page'),
+    url(r'^(?P<page_number>\d+)/$', TagPageList.as_view(), name='organizer_tag_page'),
     url(r'^create/$', TagCreate.as_view(), name='organizer_tag_create'),
-    url(r'(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
+    url(r'^(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
     url(r'^(?P<slug>[\w\-]+)/update/$', TagUpdate.as_view(), name='organizer_tag_update'),
     url(r'^(?P<slug>[\w\-]+)/delete/$', TagDelete.as_view(), name='organizer_tag_delete'),
 ]
