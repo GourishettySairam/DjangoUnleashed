2ea9a391dc942332217afbe0c0f02e41d336c85c
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-05-22 23:03:20 +0530

    replacing flat pages

--- suorganizer/suorganizer/settings.py
+++ suorganizer/suorganizer/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.flatpages',
     'organizer',
     'blog',
     'contact',
@@ -59,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'suorganizer.urls'
--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -25,21 +25,20 @@ from organizer.urls import  (
 from .views import redirect_root
 from contact import urls as contact_urls
 from blog import urls as blog_urls
-from django.views.generic import RedirectView
-
-from django.contrib.flatpages import \
-    urls as flatpage_urls
+from django.views.generic import RedirectView, TemplateView
 
 urlpatterns = [
+    url(r'^about/$', TemplateView.as_view(
+        template_name='site/about.html'
+    ), name='about_site'),
     path('admin/', admin.site.urls),
     url(r'^blog/', include(blog_urls)),
     url(r'^newslink/', include(newslink_urls)),
     url(r'^startup/', include(startup_urls)),
     url(r'^tag/', include(tag_urls)),
     url(r'^contact/', include(contact_urls)),
-    url(r'^', include(flatpage_urls)),
     url(r'^$', RedirectView.as_view(
         pattern_name='blog_post_list',
         permanent=False
-    ))
+    )),
 ]
