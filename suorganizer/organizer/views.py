b50f8a6bd04aa77a39ca4c87e224814bb68fb20b
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-08-28 22:19:20 +0530

    startup list pagination

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -10,9 +10,10 @@ from django.views.generic import View
 
 # from .utils import ObjectDeleteMixin, ObjectUpdateMixin
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import DetailView, CreateView, DeleteView
+from django.views.generic import DetailView, CreateView, DeleteView, ListView
 
 from core.utils import UpdateView
+from .utils import PageLinksMixin
 
 # Create your views here.
 
@@ -94,56 +95,10 @@ class StartupDetail(DetailView):
 class TagDetail(DetailView):
     model = Tag
 
-class StartupList(View):
-    page_kwarg = 'page'
-    paginate_by = 5
-    template_name = 'organizer/startup_list.html'
+class StartupList(PageLinksMixin, ListView):
+    model = Startup
+    paginate_by: 5
 
-    def get(self, request):
-        startups = Startup.objects.all()
-        paginator = Paginator(startups, self.paginate_by)
-        page = None
-        page_number = request.GET.get(
-            self.page_kwarg
-        )
-        prev_url = None
-        next_url = None
-        try:
-            page = paginator.page(page_number)
-        except PageNotAnInteger:
-            print(paginator.page(1))
-            page = paginator.page(1)
-        except EmptyPage:
-            page = paginator.page(
-                paginator.num_pages
-            )
-        if page.has_previous():
-            prev_url = "?{pkw}={n}".format(
-                pkw=self.page_kwarg,
-                n = page.previous_page_number()
-            )
-        else:
-            prev_url = None
-        if page.has_next():
-            next_url = "?{pkw}={n}".format(
-                pkw=self.page_kwarg,
-                n=page.next_page_number()
-            )
-        else:
-            next_url = None
-        
-        context = {
-            'is_paginated': page.has_other_pages(),
-            'paginator': paginator,
-            'startup_list': page,
-            'next_page_url': next_url,
-            'previous_page_url': prev_url
-        }
-        return render(
-            request,
-            self.template_name,
-            context
-        )
 
 def startup_detail(request, slug):
     startup = get_object_or_404(Startup, slug__iexact=slug)
