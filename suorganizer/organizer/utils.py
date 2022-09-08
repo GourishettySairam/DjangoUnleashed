f117651ac37f83da5548fce123de56447d0c4021
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-09-08 23:43:35 +0530

    extending pagination behaviour further

--- suorganizer/organizer/utils.py
+++ suorganizer/organizer/utils.py
@@ -72,6 +72,17 @@ class ObjectDeleteMixin:
 class PageLinksMixin:
     page_kwarg = 'page'
 
+    def first_page(self, page):
+        if page.number > 1: 
+            return self._page_urls(1)
+        return None
+    
+    def last_page(self, page):
+        last_page = page.paginator.num_pages
+        if page.number < last_page:
+            return self._page_urls(last_page)
+        return None
+
     def _page_urls(self, page_number):
         return "?{pkw}={n}".format(
             pkw=self.page_kwarg,
@@ -79,15 +90,17 @@ class PageLinksMixin:
         )
     
     def previous_page(self, page):
-        if page.has_previous():
+        if (page.has_previous()
+            and page.number > 2):
             return self._page_urls(
                 page.previous_page_number()
             )
         return None
     
     def next_page(self, page):
-        if page.has_next():
-            return self.page_urls(
+        last_page = page.paginator.num_pages
+        if (page.has_next() and page.number < last_page -1):
+            return self._page_urls(
                 page.next_page_number()
             )
         return None
