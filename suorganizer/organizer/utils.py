16f9b134a51e97153cbd62b916d1cbe23bc5e55f
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-09-25 01:04:08 +0530

    created mixins

--- suorganizer/organizer/utils.py
+++ suorganizer/organizer/utils.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.views.generic import View
 from django.db.models import Model
 from django.core.exceptions import ImproperlyConfigured
+from .models import Startup, NewsLink
 
 class ObjectUpdateMixin:
     form_class = None
@@ -116,3 +117,34 @@ class PageLinksMixin:
                 'next_page_url': self.next_page(page),
             })
         return context
+
+class StartupContextMixin():
+    startup_slug_url_kwarg = "startup_slug"
+    startup_context_object_name = "startup"
+
+    def get_context_data(self, **kwargs):
+        startup_slug = self.kwargs.get(
+            self.startup_slug_url_kwarg
+        )
+        startup = get_object_or_404(
+            Startup, slug__iexact=startup_slug
+        )
+        context = {
+            self.startup_context_object_name: startup,
+        }
+        context.update(kwargs)
+        return super().get_context_data(**context)
+    
+class NewsLinkGetObjectMixin():
+    def get_object(self, queryset=None):
+        startup_slug = self.kwargs.get(
+            self.startup_slug_url_kwarg
+        )
+        newslink_slug = self.kwargs.get(
+            self.slug_url_kwarg
+        )
+        return get_object_or_404(
+            NewsLink,
+            slug__iexact=newslink_slug,
+            startup__slug__iexact=startup_slug
+        )
